f5074945f7d5397a7716c9444b4b6cc92787a946
mikejonesyjones <50116686+mikejonesyjones@users.noreply.github.com>
2025-09-25 21:28:03 +0100

    Went a bit crazy thinking about retry strategies

--- api/src/main/java/com/reliaquest/api/employee/README.md
+++ api/src/main/java/com/reliaquest/api/employee/README.md
@@ -12,3 +12,17 @@ The delete on the API seems dodgy; on the client side it says it will accept an 
 I reassessed my next steps, that request limit interceptor is a tricky little so-and-so...how the hell will I create a fair back off policy for a server that will randomly need between 30 and 90 seconds backoff every 5 to 10 attempts!???
 
 Figure my next step has to be to get an integration test working, so I can test...whatever the hell I come up with...end-to-end.
+
+So I wrote a test harness thinking it might help me work out what a sensible retry strategy would be...I suppose it did help me realise how wrong my thinking was. Normally I've either been sent a Retry-After header back or else the limits were specified in the API documentation; in any case, it's been me throttling jobs in our code to avoid bombarding other people's APIs. In this exercise, I'm fulfilling my API requests by directly calling another API...
+
+I had the code in front of me all along telling me the server will only handle at best 10 requests every ~30 seconds and worst 5 requests every ~90 seconds. Trying to deal with much more than that except in short bursts is futile, there must come a point where I'm just delaying giving people the inevitable news: 429!
+
+Worst thing I could do is fall over because I just kept accepting requests myself even though the server would never be able to deal with them in any reasonable time.
+
+Something else that is bothering me is how it can be unfair, I might have requests that are waiting to be retried and fresh one could come in and steal its spot causing it to take longer or fail altogether.
+
+I suppose my retry strategy would need to be smarter and have some sort of state tracking how many retries are currently ongoing. Past a certain point, unless I'm willing to make way more attempts, there's no point holding on to them longer.
+
+For example, if I've already got 30 requests waiting, I know that at best I can fulfil those in ~1.5 minutes, if my system is fair and they will complete before the new request, there's no point making the new request retry at all if it isn't going to retry past 90 seconds.
+
+I think I need to stop before I go crazy! This was fun though.
--- api/src/test/java/com/reliaquest/api/TestUtils.java
+++ api/src/test/java/com/reliaquest/api/TestUtils.java
@@ -4,7 +4,16 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
 import java.nio.file.Files;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Stream;
 
 public final class TestUtils
 {
@@ -22,5 +31,72 @@ public final class TestUtils
         ClassPathResource resource = new ClassPathResource(filename);
         return Files.readString(resource.getFile()
                                         .toPath());
+    }
+
+    public static int findRandomFreePort() throws IOException
+    {
+        try (ServerSocket socket = new ServerSocket(0))
+        {
+            socket.setReuseAddress(true);
+            return socket.getLocalPort();
+        }
+    }
+
+    /**
+     * My personal laptop is too old to run Docker, so couldn't use TestContainers :(
+     */
+    public static CompletableFuture<Process> startServer(String jarPath, int port, String readyLogLine,
+                                                         String... extraArgs)
+    {
+        return CompletableFuture.supplyAsync(() -> {
+            try
+            {
+                final String[] allArgs = Stream.concat(Stream.of("java", "-jar", jarPath, "--server.port=" + port),
+                                                       Stream.of(extraArgs))
+                                               .toArray(String[]::new);
+                final ProcessBuilder pb = new ProcessBuilder(allArgs);
+                pb.redirectErrorStream(true);
+                final Process process = pb.start();
+
+                final CompletableFuture<Void> readyFuture = new CompletableFuture<>();
+                new Thread(() ->{
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream())))
+                    {
+                        String line;
+                        while ((line = reader.readLine()) != null)
+                        {
+                            System.out.println(line);
+                            if (line.contains(readyLogLine))
+                            {
+                                readyFuture.complete(null);
+                            }
+                        }
+                    }
+                    catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }).start();
+                readyFuture.get(60, TimeUnit.SECONDS);
+
+                return process;
+            }
+            catch (IOException | InterruptedException | ExecutionException | TimeoutException e)
+            {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public static void stopServer(Process process) throws InterruptedException
+    {
+        process.destroy();
+        final boolean exited = process.waitFor(5, TimeUnit.SECONDS);
+
+        if (!exited)
+        {
+            process.destroyForcibly();
+            process.waitFor();
+        }
     }
 }
